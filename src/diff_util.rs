1935744348af4357964b0a3329744134976670eb
Isaac M. Good <imgoodproductions@sbcglobal.net>
2025-12-14 01:23:24 -0800

    Fix pendantic Clippy lints in diff_util.rs
    Separating out these PRs by individual module (except where changes
    had to cross-module boundaries) to make them manageable.

--- src/diff_util.rs
+++ src/diff_util.rs
@@ -2,15 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 use colorful::{Color, Colorful};
 use diff::Result;
+use std::fmt::Write;
 
 pub fn human_diff_lines<L: AsRef<str>, R: AsRef<str>>(left: L, right: R) -> String {
     let mut output = String::new();
     for diff in diff::lines(left.as_ref(), right.as_ref()) {
         match diff {
-            Result::Left(value) => output.push_str(&format!("{}", format!("-{}\n", value).color(Color::Red))),
-            Result::Both(value, _) => output.push_str(&format!(" {}\n", value)),
-            Result::Right(value) => output.push_str(&format!("{}\n", format!("+{}", value).color(Color::Blue))),
+            Result::Left(value) => writeln!(output, "{}", format!("-{value}").color(Color::Red)),
+            Result::Both(value, _) => writeln!(output, " {value}"),
+            Result::Right(value) => writeln!(output, "{}", format!("+{value}").color(Color::Blue)),
         }
+        .expect("output is a string");
     }
     output
 }
